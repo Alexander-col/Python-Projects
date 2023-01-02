fe005a8aba81f14be5aae3d03515ce18eb14d2c2
Alexander-col <aexro2221@gmail.com>
2023-01-02 17:38:41 -0500

    Ascii art file started!

---
+++ ascii_art.py
@@ -0,0 +1,15 @@
+# Author: Alexander Colotl 
+# Date: Jan 3rd, 2023
+# Goal: TO have an otion of three icons to display on which will be Miku, Splatoon and finally the Overwatch logo!
+# Notes for Jess: I hope you found the program funny and it actually works. Also please tell me I did better commenting than you. Okay bye <3
+
+print("Welcome to the ascii art program! \nYou look so good, can't wait to show you art!") 
+# Basic print introduction statment
+
+name = input("But first just curious, what's your name? ")
+# Declare name and input with it 
+
+def miku_art():
+    print("The def function worked")
+
+miku_art()
